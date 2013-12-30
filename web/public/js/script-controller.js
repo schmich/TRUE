e14bfc215305798891225d9e77038bf9fc929313
Chris Schmich <schmch@gmail.com>
2013-12-30 00:37:28 -0800

    Fixing display of vars.

--- web/public/js/script-controller.js
+++ web/public/js/script-controller.js
@@ -113,7 +113,9 @@ function ScriptCtrl($scope) {
 }
 
 // Examples:
-// even/odd:  `$2/2*=$["even"]?~["odd"]?
+// even/odd:  [$2/2*=]even:[even;!~]odd:6odd;!
+// even/odd (mutual recursion):
+//   [$0=$[1_\@%]?~[1-odd;!]?]even:[$0=~$[1_\@%]?~[1-even;!~]?]odd:6even;!
 // factorial: [$1=$[\%1\]?~[$1-f;!*]?]f:`f;!.
 // strrev:    0[^$~][]#%[$][,]#
 // strlen:    0[^~][1+]#.
