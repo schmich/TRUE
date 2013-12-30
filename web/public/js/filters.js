3746994247691d5264e2c8b37bedf37970c33112
Chris Schmich <schmch@gmail.com>
2013-12-30 01:50:23 -0800

    s/next/active/

--- web/public/js/filters.js
+++ web/public/js/filters.js
@@ -15,23 +15,23 @@ $app.filter('highlight', function($sce) {
     if (!command)
       return src;
 
-    var next = command.source;
+    var active = command.source;
 
     // This is really inefficient. We could cache this info somewhere.
     var lines = src.split("\n");
     var index = 0;
-    for (var i = 0; i < next.line - 1; ++i) {
+    for (var i = 0; i < active.line - 1; ++i) {
       index += lines[i].length + 1;
     }
 
-    index += next.column;
+    index += active.column;
 
     // TODO: Escape HTML.
     var html = src.substr(0, index - 1) 
-             + '<span class="next-command">'
-             + next.text
+             + '<span class="active-command">'
+             + active.text
              + '</span>'
-             + src.substr(index + next.text.length - 1);
+             + src.substr(index + active.text.length - 1);
 
     return $sce.trustAsHtml(html);
   };
