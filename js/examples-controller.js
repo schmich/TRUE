bde955717506b9273f8307dc3a2b1e2101ca2471
Chris Schmich <schmch@gmail.com>
2014-01-15 14:10:45 -0800

    Adding more examples.

--- js/examples-controller.js
+++ js/examples-controller.js
@@ -55,24 +55,25 @@ app.controller('ExamplesCtrl', function($scope, $http) {
       name: 'Simple add',
       script: '[[$][1-\\1+\\]#%]add:\n5 7add;!.',
       description: 'Implements addition using only increment (+1) and decrement (-1) operations.'
+    },
+    {
+      name: 'Parse integer',
+      script: '[$$47>\\58\\>&]d:\n[0[^d;!][\\10*\\48-+]#%]r:\nr;!r;!+.',
+      input: '32 10',
+      description: 'Reads two integers from input and adds them.'
+    },
+    {
+      name: 'Read all input',
+      script: '0[^$~][]#%',
+      input: 'hello',
+      description: 'Reads all input onto the stack, terminated with a zero.'
+    },
+    {
+      name: 'Star square',
+      script: "[1+$[1-$]['*,]#%%]b: { draws top/bottom border }\n[$$1=$[\"*\"%%]?~[ { handle corner case of size 1 }\n b;! { draw top border }\n $$1-[1-$] { for each open row }\n  [\\10,'*,$1-[1-$][' ,]#%\\'*,] { print open row }\n  #%%10, { print trailing newline }\n b;! { draw bottom border }\n]?]d:\n7d;!",
+      description: 'Draws a 7x7 ASCII square made of stars.'
     }
   ];
-
-// Examples:
-// [0[^$~][]#%]readAll:
-// [$$47>\58\>&]isDigit:
-// [0[^isDigit;!][\10*\48-+]#%]readInt:
-
-// draw box (size n):
-// [1+$[1-$]['*,]#%%]db: { define top/bottom border func }
-// ` { read size }
-// $$1=$["*"%%]?~[ { handle corner case of size 1 }
-//  db;! { draw top border }
-//  $$1-[1-$] { for each open row }
-//   [\10,'*,$1-[1-$][' ,]#%\'*,] { print open row }
-//  #%%10, {end for each, trailing newline }
-//  db;! { draw bottom border }
-// ]?
 
   $scope.exampleLink = function(example) {
     var input = example.input;
