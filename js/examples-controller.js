e09f67bf989e86617a52722e4cef934b3e55cf55
Chris Schmich <schmch@gmail.com>
2014-01-17 21:55:09 -0600

    Moving examples to an external source (gist.github.com).

--- js/examples-controller.js
+++ js/examples-controller.js
@@ -1,9 +1,16 @@
 var app = require('./app');
 var base64 = require('urlsafe-base64');
-var examples = require('./examples');
 
 app.controller('ExamplesCtrl', function($scope, $http) {
-  $scope.examples = examples;
+  $scope.examples = [];
+
+  $http.get('https://api.github.com/gists/8480925')
+    .success(function(data) {
+      $scope.examples = $.parseJSON(data.files['TRUE.json'].content);
+    })
+    .error(function(data, status) {
+    });
+
   $scope.exampleLink = function(example) {
     var input = example.input;
     var script = example.script;
